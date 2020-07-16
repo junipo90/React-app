6376e6d944c9e8a1d16f5e6c27a0ace6ac936c2b
이읍준 <66265293+junipo90@users.noreply.github.com>
2020-07-17 00:15:37 +0900

    master

---
+++ src/components/TOC.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    render() {
+        var lists = [];
+        var data = this.props.data;
+        for (let i = 0; i < data.length; i++) {
+            lists.push(<li key={data[i].id}>
+                <a
+                    href={"/content/" + data[i].id}
+                    data-id={data[i].id}
+                    onClick={function (e) {
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }.bind(this)}
+                >{data[i].title}</a></li>)
+        }
+        return (
+            <nav>
+                <ul>
+                    {lists}
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default TOC;
